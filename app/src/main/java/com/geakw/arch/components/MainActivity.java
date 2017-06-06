2566c617dd0f077b43fcaa1ca220394bb7b65162
wangliqiang <liqiang1@eastmoney.com>
2017-06-06 18:15:19 +0800

    添加新工程

---
+++ app/src/main/java/com/geakw/arch/components/MainActivity.java
@@ -0,0 +1,13 @@
+package com.geakw.arch.components;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
